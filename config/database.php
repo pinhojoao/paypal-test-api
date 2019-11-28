f6df3046450cbcb2829466e7a6bfc93590d112f2
João Pinho <pinhojoao@gmail.com>
2019-11-28 10:10:11 -0300

    Alterando configuracao do pgsql

--- config/database.php
+++ config/database.php
@@ -55,14 +55,10 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('DATABASE_URL'),
             'charset' => 'utf8',
             'prefix' => '',
+            'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
